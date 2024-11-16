93dcbc9cfc2143324c5de9fae4ce05ff69917d67
cysbyte <chenyunshu2@gmail.com>
2024-11-16 23:41:51 +0800

    fix: fixed bugs

--- src/components/home/footer/index.tsx
+++ src/components/home/footer/index.tsx
@@ -5,94 +5,28 @@ import bg from '@/assets/home/footer/bg.svg'
 const Footer = () => {
   const Wrapper = styled.section`
     .container {
+      position: relative;
       display: block;
       @media screen and (max-width: 800px) {
         display: none;
       }
-      width: 100%;
-    margin-top: 6rem;
-    @media screen and (max-width: 800px) {
-      margin-top: 3rem;
-    }
-    background-image: url('${bg}');
-      .footer-container {
-        max-width: 1440px;
-        margin: auto;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        align-items: center;
-        padding: 2.5rem 14.5rem 2.5rem 8.5rem;
-        @media screen and (max-width: 800px) {
-            flex-direction: column;
-            padding: 1rem 1rem;
-            gap: 0.7rem;
-            align-items: start;
-        }
-        
-        .copyright-box {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          gap: 0rem;
-          @media screen and (max-width: 800px) {
-            gap: 0.1rem;
-          }
-          img {
-              width: auto;
-              @media screen and (max-width: 800px) {
-                width: 11px;   
-              }
-          }
-          p {
-            font-family: Ubuntu;
-            font-size: 16px;
-            font-weight: 400;
-            line-height: 18.38px;
-            text-align: left;
-            text-underline-position: from-font;
-            text-decoration-skip-ink: none;
-            color: white;
-            @media screen and (max-width: 800px) {
-              font-family: Ubuntu;
-              font-size: 10px;
-              font-weight: 400;
-              line-height: 11.49px;
-              text-align: left;
-              text-underline-position: from-font;
-              text-decoration-skip-ink: none;
-          }
-          }
-        }
-        a {
-          font-family: Ubuntu;
-          font-size: 16px;
-          font-weight: 700;
-          line-height: 18.38px;
-          text-align: left;
-          text-underline-position: from-font;
-          text-decoration-skip-ink: none;
-          text-decoration: none;
-          color: white;
-          @media screen and (max-width: 800px) {
-              font-family: Ubuntu;
-              font-size: 10px;
-              font-weight: 400;
-              line-height: 11.49px;
-              text-align: left;
-              text-underline-position: from-font;
-              text-decoration-skip-ink: none;
-          }
-        }
-      
-      }
     }
     .container-mobile {
+      position: relative;
       display: none;
       @media screen and (max-width: 800px) {
         display: block;
       }
+    }
+    .bg-img {
+      position: absolute;
+      z-index: -1;
       width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+    }
+    width: 100%;
     margin-top: 6rem;
     @media screen and (max-width: 800px) {
       margin-top: 3rem;
@@ -169,12 +103,11 @@ const Footer = () => {
         }
       
       }
-    }
-    
     `
   return (
     <Wrapper>
       <div className='container'>
+        <img className='bg-img' src={bg} alt="" />
         <div className='footer-container'>
           <div className='copyright-box'>
             <img src={copyright} alt="" />
@@ -184,6 +117,7 @@ const Footer = () => {
         </div>
       </div>
       <div className='container-mobile'>
+        <img className='bg-img' src={bg} alt="" />
         <div className='footer-container'>
           <a href="mailto:dtcmatrixsupport1@gmail.com">dtcmatrixsupport1@gmail.com</a>
           <div className='copyright-box'>
