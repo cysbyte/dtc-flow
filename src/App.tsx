c2587fa2919daa0396ccbc595fe3ed0e10f81a73
ware <morrisware01@gmail.com>
2024-11-18 14:20:59 +0800

    feat: modify HashRouter to adapt OSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import PageNotFound from './pages/PageNotFound';
 import "@fontsource/ubuntu"; // Defaults to weight 400
